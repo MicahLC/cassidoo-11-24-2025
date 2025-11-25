f72fb4600bf7835749a85661cb0630d0f77647be
Micah Lapping-Carr <MicahLC@users.noreply.github.com>
2025-11-25 17:32:36 -0600

    Add another test
    Also build might work now? If it didn't before.

--- MealTests.cs
+++ MealTests.cs
@@ -15,17 +15,30 @@ namespace cassidoo_11_24_2025
                 new MealTask("Prep Salad", 12, 13)
             ]);
             plan.Count.Should().Be(3);
-            plan.ChosenTasks.Count.Should().Be(3);
+            plan.ChosenTasks.Should().BeEquivalentTo(["Make Gravy", "Mash Potatoes", "Prep Salad"]);
         }
 
         [TestMethod]
         public void TestBasic()
         {
-            MealPlan plan = MealScheduler.MaxMealPrepTasks(new MealTask[] { 
-                new MealTask("Wake Up")
-            });
+            MealPlan plan = MealScheduler.MaxMealPrepTasks([
+                new MealTask("Wake Up", 1, 2)
+            ]);
             plan.Count.Should().Be(1);
             plan.ChosenTasks.Should().BeEquivalentTo(["Wake Up"]);
+        }
+
+        [TestMethod]
+        public void TestSimpleMax()
+        {
+            MealPlan plan = MealScheduler.MaxMealPrepTasks([
+                new MealTask("Buy Eggs", 10, 14),
+                new MealTask("Make Cookies", 11, 12),
+                new MealTask("Bake Bread", 12, 13),
+                new MealTask("Bake Rolls", 13, 14)
+            ]);
+            plan.Count.Should().Be(3);
+            plan.ChosenTasks.Should().BeEquivalentTo(["Make Cookies", "Bake Bread", "Bake Rolls"]);
         }
     }
 }
